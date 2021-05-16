5b33d81b8065f28a27e47cf72ae229bb52020c67
Yelena <lena.skripnik.01@gmail.com>
2021-05-16 12:58:27 +0300

    Create signin_page.dart

---
+++ lib/pages/signin_page.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  bool _toggleVisibility = true;
+
+  GlobalKey<FormState> _formKey = GlobalKey();
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  String _password;
+  String _email;
+
+  Widget _buildEmailTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: "Email",
+        hintStyle: TextStyle(
+          color: Color(0xFFBDC2CB),
+          fontSize: 18.0,
+        ),
+      ),
+      onSaved: (String email) {
+        _email = email.trim();
+      },
+      validator: (String email) {
+        String errorMessage;
+        if (!email.contains("@")) {
+          errorMessage = "Invalid email";
+        }
+        return errorMessage;
+      },
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: "Password",
+        hintStyle: TextStyle(
+          color: Color(0xFFBDC2CB),
+          fontSize: 18.0,
+        ),
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              _toggleVisibility = !_toggleVisibility;
+            });
+          },
+          icon: _toggleVisibility
+              ? Icon(Icons.visibility_off)
+              : Icon(Icons.visibility),
+        ),
+      ),
+      obscureText: _toggleVisibility,
+      onSaved: (String password) {
+        _password = password;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        //backgroundColor: Colors.yellow[800],
+        key: _scaffoldKey,
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                    image: AssetImage('assets/images/main.jpg'),
+                  height: 250.0,
+                ),
+                Text(
+                  "Sign In",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Roboto',
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Card(
+                  elevation: 5.0,
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: <Widget>[
+                        _buildEmailTextField(),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        _buildPasswordTextField(),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Forgotten Password?",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.yellow[800],
+                        fontFamily: 'Roboto',
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                _buildSignInButton(),
+                Divider(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                          color: Color(0xFFBDC2CB),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0),
+                    ),
+                    SizedBox(width: 10.0),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (BuildContext context) => SignUpPage()));
+                      },
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(
+                            color: Colors.yellow[800],
+                            fontFamily: 'Roboto',
+                            fontSize: 18.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignInButton() {
+    return ScopedModelDescendant(
+      builder: (BuildContext sctx, Widget child, MainModel model) {
+        return GestureDetector(
+          onTap: () {
+            showLoadingIndicator(context, "Signing in...");
+            onSubmit(model.authenticate);
+          },
+          child: Container(
+            width: 350.0,
+            padding: EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                color: Colors.yellow[800],
+                  borderRadius: BorderRadius.circular(12.0)
+              ),
+              child: Text("Sign in",
+              textAlign: TextAlign.center,
+              style: new TextStyle(
+                color: Colors.white,
+                fontSize: 25.0,
+                fontFamily: 'Roboto'
+              ),
+              ),
+          ),
+        );
+      },
+    );
+  }
+
+  void onSubmit(Function authenticate) {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+
+      authenticate(_email, _password).then((final response) {
+        if (!response['hasError']) {
+          Navigator.of(context).pop();
+          Navigator.of(context).pushReplacementNamed("/mainscreen");
+        } else {
+          Navigator.of(context).pop();
+          _scaffoldKey.currentState.showSnackBar(
+            SnackBar(
+              duration: Duration(seconds: 2),
+              backgroundColor: Colors.red,
+              content: Text(response['message']),
+            ),
+          );
+        }
+      });
+    }
+  }
+}
