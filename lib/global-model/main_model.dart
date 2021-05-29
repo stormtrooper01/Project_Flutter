b9ac6d46b5c9adcc1c9088352e1a70b9c093e292
Yelena <lena.skripnik.01@gmail.com>
2021-05-29 14:53:10 +0300

    Delete lib/global-model directory

--- lib/global-model/main_model.dart
+++
@@ -1,9 +0,0 @@
-import '../scoped-model/food_model.dart';
-import 'package:scoped_model/scoped_model.dart';
-
-class MainModel extends Model with FoodModel, UserModel {
-  void fetchAll() {
-    fetchFoods();
-    fetchUserInfos();
-  }
-}
