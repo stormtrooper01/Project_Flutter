102b2a170f26171f5694e3623605466f73454770
Yelena <lena.skripnik.01@gmail.com>
2021-06-18 14:15:20 +0300

    Update main_model.dart

--- lib/scoped-model/main_model.dart
+++ lib/scoped-model/main_model.dart
@@ -1,6 +1,6 @@
 import 'package:scoped_model/scoped_model.dart';
 import '../scoped-model/user_scoped_model.dart';
-//import '../scoped-model/food_model.dart';
+import '../scoped-model/food_model.dart';
 
 class MainModel extends Model with FoodModel, UserModel {
   void fetchAll() {
